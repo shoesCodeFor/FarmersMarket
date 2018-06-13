8aa787a89695bf6db21f5a975dd6bc7e53dbd2f8
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-06-13 12:16:53 -0600

    Refactor as USDA client

---
+++ app_logic.js
@@ -0,0 +1,3 @@
+/*
+ *  This JS file will hold logic for FarmersMarket.  It combines functions found in MapQuest_Client_V2.js and USDA_Clients.js to build functionality for the FarmersMarket Bootstrap implementation.
+ */
