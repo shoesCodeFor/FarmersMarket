a0b157e6f00c8f0d02c11884a0932801b45b99bf
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-11-12 13:39:27 -0700

    Test suite for geo

--- test/Gecocoding_spec.js
+++ test/Gecocoding_spec.js
@@ -1,3 +1,25 @@
 const chai, {expect} = require('chai');
 const mocha = require('mocha');
 
+describe("Geocoding test suite", () => {
+    describe("Return the user's location", () => {
+        it("Should return a lat/long pair when the navigator function is called", done =>{
+
+        });
+    });
+    describe("Return a single geocode location", () => {
+        it("Should Return a lat/long for a geocode location", done => {
+
+        });
+    });
+    describe("Return a batch of geocode locations", () => {
+        it("Should Return an array of lat/long for the geocode locations", done => {
+
+        });
+    });
+    describe("Return a path from point A to point B", () =>{
+        it("Should return a directional path", done =>{
+
+        });
+    });
+}); 
