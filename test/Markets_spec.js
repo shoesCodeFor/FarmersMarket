bd3e1b4f88d57763eba39a30b81206f704f721e2
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-11-13 22:39:55 -0700

    Details in the specs

--- test/Markets_spec.js
+++ test/Markets_spec.js
@@ -2,9 +2,14 @@ const chai, {expect} = require('chai');
 const mocha = require('mocha');
 
 describe("USDA Markets Test Suite", () => {
-    describe("", () =>{
-        it("", done => {
-
+    describe("Markets by Postal Code", () =>{
+        it("Should return a list of markets in proximity to the postal code", done => {
+            done();
+        });
+    });
+    describe("Markets by Lat/Long", () =>{
+        it("SHould return a list of markets in proximity to the Lat/Lng", done => {
+            done();
         });
     });
 });
