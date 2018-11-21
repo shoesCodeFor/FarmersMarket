b089b313b4f0548ccb91392a6eff689a1bb5a962
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-11-20 23:02:33 -0700

    NPM test script, should work?

--- test/Gecocoding_spec.js
+++ test/Gecocoding_spec.js
@@ -1,13 +1,20 @@
-const chai, {expect} = require('chai');
+const chai, expect = require('chai');
 const mocha = require('mocha');
+const MQ_Client = require('../MapQuest_Client_V2');
 
 describe("Geocoding test suite", () => {
     describe("Return the user's location", () => {
         it("Should return a lat/long pair when the navigator function is called", done =>{
+            MQ_Client.findMyLocation();
+            console.log(userLocation);
+        });
+    });
+    describe("Return a single line geocode location", () => {
+        it("Should Return a lat/long for a geocode location", done => {
 
         });
     });
-    describe("Return a single geocode location", () => {
+    describe("Return a single 5 box geocode location", () => {
         it("Should Return a lat/long for a geocode location", done => {
 
         });
